c43ff420bf632d7c11802e769919d2e8f4aa5625
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-03-05 03:40:07 +0000

    css

--- index.mjs
+++ index.mjs
@@ -86,7 +86,7 @@ async function onRequest(req, res) {
 
       resBody = resBody.replaceAll('hostTarget', 'hostProxy');
 
-      resBody = resBody.replace('<head>','<head><script src="https://patrick-ring-motive.github.io/de-wiki/static/links.js"></script><link rel="stylesheet" href="https://patrick-ring-motive.github.io/de-wiki/static/mods.js">');
+      resBody = resBody.replace('<head>','<head><script src="https://patrick-ring-motive.github.io/de-wiki/static/links.js"></script><link rel="stylesheet" href="https://patrick-ring-motive.github.io/de-wiki/static/mods.css">');
       
       res.end(resBody);
 
