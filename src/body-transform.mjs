75a7c21de359a5b0310880ae765f9d89ba756d29
weblet <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-03-06 21:14:44 +0000

    de-wiki.vercel.app

--- src/body-transform.mjs
+++ src/body-transform.mjs
@@ -1,16 +1,16 @@
 
-export default function transformBody(resBody,ct,hostWiki,hostTarget,hostProxy){
+export default function transformBody(resBody, ct, hostWiki, hostTarget, hostProxy) {
 
-if ((ct.indexOf('javascript') > -1)) {
-       resBody = resBody.replaceAll(hostWiki, hostProxy);
+  if ((ct.indexOf('javascript') > -1)) {
+    resBody = resBody.replaceAll(hostWiki, hostProxy);
 
-        resBody = resBody.replaceAll(hostTarget, hostProxy);
-      }
+    resBody = resBody.replaceAll(hostTarget, hostProxy);
+  }
 
 
-      resBody = resBody.replace('<head>', `<head modified><modified/>
-      <script src="https://patrick-ring-motive.github.io/de-wiki/static/links.js"></script>
-      <link rel="stylesheet" href="https://patrick-ring-motive.github.io/de-wiki/static/mods.css">`);
+  resBody = resBody.replace('<head>', `<head modified><modified/>
+      <script src="https://de-wiki.vercel.app/de-wiki/static/links.js"></script>
+      <link rel="stylesheet" href="https://de-wiki.vercel.app/de-wiki/static/mods.css">`);
 
 
   return resBody;
