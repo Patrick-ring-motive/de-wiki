258ea69090549695fdbf1a46dc70fbfd2a2f357a
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-31 21:06:19 -0500

    style: js-beautify formatting (src/body-transform.mjs)

--- src/body-transform.mjs
+++ src/body-transform.mjs
@@ -1,4 +1,3 @@
-
 export default function transformBody(resBody, ct, hostWiki, hostTarget, hostProxy) {
 
   if ((ct.indexOf('javascript') > -1)) {
@@ -7,11 +6,9 @@ export default function transformBody(resBody, ct, hostWiki, hostTarget, hostPro
     resBody = resBody.replaceAll(hostTarget, hostProxy);
   }
 
-
   resBody = resBody.replace('<head>', `<head modified>
       <script src="https://redirect-nodemjs.weblette.repl.co/de-wiki/static/links.js"></script>
       <link rel="stylesheet" href="https://redirect-nodemjs.weblette.repl.co/de-wiki/static/mods.css">`);
-
 
   return resBody;
 }
